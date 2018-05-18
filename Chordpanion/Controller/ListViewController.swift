679b09a2da5412c477fecea4fb7b1eb2d5de6ac9
Will Carhart <wcarhart@sandiego.edu>
2018-05-17 21:44:39 -0700

    Got a good start on the table view working

--- Chordpanion/Controller/ListViewController.swift
+++ Chordpanion/Controller/ListViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class ListViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
     var inputKey: String!
     var inputClassification: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    }
-
-    @IBAction func goBackButtonPressed(_ sender: UIButton) {
-        performSegue(withIdentifier: "unwindToNewProgression", sender: nil)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(UINib(nibName: "ProgressionCell", bundle: Bundle.main), forCellReuseIdentifier: "ProgressionCell")
     }
     
+}
+
+extension ListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // TODO: determine data source - database? static? probably should be static for now
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell: ProgressionTableViewCell! = tableView.dequeueReusableCell(withIdentifier: "ProgressionCell", for: indexPath) as? ProgressionTableViewCell
+        
+        if cell == nil {
+            tableView.register(UINib(nibName: "ProgressionCell", bundle: nil), forCellReuseIdentifier: "ProgressionCell")
+            cell = tableView.dequeueReusableCell(withIdentifier: "ProgressionCell") as? ProgressionTableViewCell
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        // TODO: figure out how big this should be
+        return 200.0
+    }
 }
